dc8776cfc648e492233031543f0b42c4e49b86a5
Manohar Kuse <mpkuse@connect.ust.hk>
2019-06-16 16:53:42 +0800

    ready to merge with master, the contribution, making it independent of cerebro. so long as cerebro LoopEdge msg is same as this LoopEdge msg everything should work fine.

--- src/keyframe_pose_graph_slam_node.cpp
+++ src/keyframe_pose_graph_slam_node.cpp
@@ -1447,7 +1447,7 @@ int main( int argc, char ** argv)
     // std::thread th_slam( &PoseGraphSLAM::new_optimize6DOF, slam );
 
     slam->reinit_ceres_problem_onnewloopedge_optimize6DOF_enable();
-    slam->reinit_ceres_problem_onnewloopedge_optimize6DOF_disable();
+    // slam->reinit_ceres_problem_onnewloopedge_optimize6DOF_disable();
     std::thread th_slam( &PoseGraphSLAM::reinit_ceres_problem_onnewloopedge_optimize6DOF, slam );
 
 
@@ -1469,7 +1469,7 @@ int main( int argc, char ** argv)
     options.line_color_style = 10;
     options.linewidth_multiplier = 3; //0.25; //8
     options.udumbe_offset_y = 30.0;
-    // std::thread th6( opt_traj_publisher_colored_by_world, manager, slam, viz, options );
+    std::thread th6( opt_traj_publisher_colored_by_world, manager, slam, viz, options );
 
 
 
@@ -1491,7 +1491,7 @@ int main( int argc, char ** argv)
     // th3.join();
     // th4.join();
     th5.join();
-    // th6.join();
+    th6.join();
 
     th_slam.join();
 
